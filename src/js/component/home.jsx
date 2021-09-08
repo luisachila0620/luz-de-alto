005c8a1e8cfd8adf6cde459503a48f886cc0f292
luisachila0620 <luisarchila038@gmail.com>
2021-09-08 01:49:54 +0000

    hola

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -7,11 +7,28 @@ import rigoImage from "../../img/rigo-baby.jpg";
 const Home = () => {
 	return (
 		<div className="container">
-			<div className="circle"></div>
-			<div className="circle"></div>
-			<div className="circle"></div>
+			<div className="circle red" color="red"></div>
+			<div className="circle yellow" color="yellow"></div>
+			<div className="circle green" color="green"></div>
 		</div>
 	);
 };
 
 export default Home;
+cons;
+
+const circles = document.querySelectorAll(".circles");
+let activelight = 0;
+
+setInterval(changelight, 1000);
+
+function changelight() {
+	circles[activelight].className = "circle";
+	activelight++;
+
+	if (activelight > 2) {
+		activelight = 0;
+	}
+	const currentlight = circles[activelight];
+	currentlight.classList.add(currentlight.getAttribute("color"));
+}
