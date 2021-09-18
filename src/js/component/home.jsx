bb47f147ab41f556e7c49edfa93b44d572b8af47
luisachila0620 <luisarchila038@gmail.com>
2021-09-18 21:00:49 +0000

    lo hicimos

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,18 +1,33 @@
-import React from "react";
-
+import React, { useState } from "react";
 
 //create your first component
 const Home = () => {
+	const [color, setColor] = useState("");
+	console.log("color", color);
 	return (
 		<div className="container">
-			<div className="circle red" color="red"></div>
-			<div className="circle yellow" color="yellow"></div>
-			<div className="circle green" color="green"></div>
+			<div
+				className={`circle red ${color == "red" ? "glow" : ""} `}
+				color="red"></div>
+			<div
+				className={`circle yellow ${color == "yellow" ? "glow" : ""} `}
+				color="yellow"></div>
+			<div
+				className={`circle green ${color == "green" ? "glow" : ""} `}
+				color="green"></div>
 			<div className="letters">
-				<button className="stoplight">red</button>
-				<button className="slowlight">yellow</button>
-				<button className="golight">green</button>
-				<div className="reset">
+				<button className="stoplight" onClick={() => setColor("red")}>
+					red
+				</button>
+				<button
+					className="slowlight"
+					onClick={() => setColor("yellow")}>
+					yellow
+				</button>
+				<button className="golight" onClick={() => setColor("green")}>
+					green
+				</button>
+				<div className="reset" onClick={() => setColor("")}>
 					<button>RESET</button>
 				</div>
 			</div>
@@ -21,6 +36,3 @@ const Home = () => {
 };
 
 export default Home;
-
-
-
